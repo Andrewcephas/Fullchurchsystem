48edd90c7dcc344dbfd861da4fde7db074e3f2a5
Andrewcephas <ngumbaucephas2@gmail.com>
2026-04-22 19:04:50 +0300

    Add Vercel config for Django + React deployment

---
+++ api/vercel.py
@@ -0,0 +1,9 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'church_backend.settings')
+
+application = get_wsgi_application()
+
+def handler(request, context):
+    return application(request, context)
